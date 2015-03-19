486b1f06249ed8de3ab5a475eeb9a503301b3401
live4code <live4code@gmail.com>
2015-03-19 02:03:21 +0000

    commit exercise content by Live4Code

--- array/check-solution.js
+++ array/check-solution.js
@@ -4,12 +4,12 @@ check("array-solution.js", "array.js", function(error, details){
     
     if(!error)
     {
-        console.log("The solution is failed, here is the difference", error, details.diff);
+        console.log("FAIL, The solution is failed, here is the difference", error, details.diff);
         
     }else
     {
         
-        console.log("Congrats, the solution is passed");
+        console.log("PASS, the solution is passed");
     }
     
 })
